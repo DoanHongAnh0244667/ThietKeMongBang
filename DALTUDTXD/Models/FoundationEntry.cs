3591202ee4938189d4622b3b6feb90a9d820bf7d
Thành - 67CSQT Nguyễn Việt <0221467@st.huce.edu.vn>
2025-05-31 23:29:22 +0700

    thêm model

---
+++ DALTUDTXD/Models/FoundationEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTUDTXD.Models
+{
+    public class FoundationEntry
+    {
+        public int Sothutulopdat { get; set; }
+        public int Sohieudiachat { get; set; }
+        public string Tenlopdat { get; set; }
+        public float Chieudaylopdat { get; set; }
+        public double Khoiluongtunhien { get; set; }
+        public double Gocmasattrong { get; set; }
+        public double Lucdinhket { get; set; }
+        public double Modunbiendang { get; set; }
+        public string Vitrimong { get; set; }
+    }
+}
